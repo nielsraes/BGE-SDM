93855fc89e0d6124c55243e871a5d19c1a21d1aa
onrcrpk <onrcrpk@gmail.com>
2022-10-24 11:48:48 +0200

    until step 6

--- SDM_BGE_Pipeline.R
+++ SDM_BGE_Pipeline.R
@@ -1,7 +1,7 @@
 rm(list = ls(all=T))
 setwd("D:/Github/BGE-SDM")
 #save(list=ls(all=TRUE), file="D:/Github/BGE-SDM/SDM.RData") # save RDATA for later use
-load("D:/Github/BGE-SDM/SDM.RData")
+#load("D:/Github/BGE-SDM/SDM.RData")
 
 #### Install required libraries ####
 library(rgbif)
@@ -18,11 +18,10 @@ countries <- rgdal::readOGR("D:/Github/BGE-SDM/GISDATA/Study Area SHP")
 #dev.off()
 proj4string(countries) <- "+proj=longlat +datum=WGS84"
 plot(countries)
-str(countries)
-countries@bbox
+str(countries); countries@bbox
 
 #Define extent
-extent <- extent(-43, 109, 25, 81) ##NEED TO BE REARRANGE
+extent <- extent(-43, 109, 25, 81) ##NEED TO BE REARRANGE FOR EAST SIDE
 
 #### 1. GBIF DATA ####
 #get the key of species by its name
@@ -44,14 +43,29 @@ map_plot(dat.data, lon = "decimalLongitude", lat = "decimalLatitude", size = 1, 
 plot(countries); points(dat.data$decimalLongitude, dat.data$decimalLatitude, pch=19, col="red")
 
 plot(countries)
-fe.gbif <- dat.data
+
+#Prepare the data
+fe.gbif <- dat.data[, c('species', 'decimalLongitude', 'decimalLatitude')]
+head(fe.gbif); dim(fe.gbif)
+duplicates <- duplicated(fe.gbif)
+fe.gbif <- fe.gbif[!duplicates,]
+head(fe.gbif); dim(fe.gbif)
+write.csv(fe.gbif, 'D:/Github/BGE-SDM/Output/Oeneis_jutta.csv', row.names=F)
+fe.gbif.maxent <- fe.gbif[, c("species", "decimalLongitude", "decimalLatitude")]
+names(fe.gbif.maxent) <- c("species", "lon", "lat")
+head(fe.gbif.maxent); dim(fe.gbif.maxent)
+plot(countries); points(fe.gbif.maxent$lon, fe.gbif.maxent$lat, pch=19, col='red')
+write.csv(fe.gbif.maxent, 'D:/Github/BGE-SDM/Output/fe.gbif.maxent.csv', row.names=F) # write species points file
+
 #assign the fe.gbif as SpatialPointsDataFrame
 coordinates(fe.gbif) <- ~decimalLongitude+decimalLatitude
 str(fe.gbif)
 #assign CRS
 P4S.latlon <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+
+
 fe.gbif@proj4string <- P4S.latlon
-plot(countries); plot(fe.gbif, col='purple', add=T)
+plot(countries); plot(fe.gbif, col='red', add=T, pch = 16)
 
 
 ### 2. Compile climate data ####
@@ -104,9 +118,8 @@ head(fe.gbif); str(fe.gbif)
 fe.gbif <- cbind(fe.gbif, fe.gbif.abiotic) # Link species col and climate data
 names(fe.gbif)
 head(fe.gbif)
-
-#error?? = duplicated() applies only to vectors
-duplicates <- duplicated(fe.gbif[,c("species", "grid.index")]) # Duplicates on grid.index
+#error?? = duplicated() applies only to vectors sol: as.data.frame()
+duplicates <- duplicated(as.data.frame(fe.gbif)[,c("species", "grid.index")]) # Duplicates on grid.index
 names(fe.gbif)
 table(duplicates)
 summary(fe.gbif)
@@ -142,21 +155,6 @@ present.df@data$mask <- as.data.frame(stack('D:/Github/BGE-SDM/Output/mask.asc')
 head(present.df); dim(present.df)
 head(fe.gbif)
 
-
-#Prepare the data for the MaxEnt
-fe.gbif <- dat.data[, c('species', 'decimalLongitude', 'decimalLatitude')]
-head(fe.gbif); dim(fe.gbif)
-duplicates <- duplicated(fe.gbif)
-fe.gbif <- fe.gbif[!duplicates,]
-head(fe.gbif); dim(fe.gbif)
-write.csv(fe.gbif, 'D:/Github/BGE-SDM/Output/Oeneis_jutta.csv', row.names=F)
-fe.gbif.maxent <- fe.gbif[, c("species", "decimalLongitude", "decimalLatitude")]
-names(fe.gbif.maxent) <- c("species", "lon", "lat")
-head(fe.gbif.maxent); dim(fe.gbif.maxent) # 103   3
-plot(countries); points(fe.gbif.maxent$lon, fe.gbif.maxent$lat, pch=19, col='red')
-
-write.csv(fe.gbif.maxent, 'D:/Github/BGE-SDM/Output/fe.gbif.maxent.csv', row.names=F) # write species points file
-
 # Convert to coll.locs spatial Points Data Frame and create 500 km buffer
 coordinates(fe.gbif.maxent) <- ~lon+lat
 proj4string(fe.gbif.maxent) <- P4S.latlon
@@ -173,11 +171,11 @@ v <- extract(mask, x@polygons, cellnumbers=T)
 str(v)
 # use rbind to combine the elements in list v
 v <- do.call(rbind, v)
-head(v); dim(v) # 211089  2
+head(v); dim(v) 
 
 # remove ocean cells
 v <- unique(na.omit(v))
-head(v); dim(v) # 56783  2
+head(v); dim(v)
 
 # to display the results
 m <- mask
@@ -221,7 +219,7 @@ x <- sample(1:(dim(present.species.df)[1]), 10000, replace=F) # sample 10k backg
 sample.df <- present.species.df[x,]
 sample.df <- present.species.df
 head(sample.df); dim(sample.df) 
-plot(countries); points(sample.df$x, sample.df$y, col='green'); plot(countries, add=T)
+#plot(countries); points(sample.df$x, sample.df$y, col='green'); plot(countries, add=T)
 sample.matrix <- as.matrix(sample.df)
 head(sample.matrix); dim(sample.matrix)
 
@@ -240,9 +238,9 @@ dim(sample.df.keep)
 names(sample.df.keep)
 
 # Species dataframe for keep.dat
-head(fe.gbif); dim(fe.gbif); str(fe.gbif) # 103  24
+head(fe.gbif); dim(fe.gbif); str(fe.gbif) 
 fe.gbif.df <- fe.gbif[, (colnames(fe.gbif) %in% keep.dat)] # retrieve species records
-head(fe.gbif.df); dim(fe.gbif.df) # 103  19
+head(fe.gbif.df); dim(fe.gbif.df) 
 fe.gbif.df$mask <- 1 # Add mask column
 names(fe.gbif.df)
 
@@ -255,7 +253,7 @@ mainDirMaxent <- "D:/Github/BGE-SDM/Output/"
 ### CHECK FOLDER NAMES !!!
 ### Logistic
 #error?
-swd <- rbind(fe.gbif.df, sample.df.keep); dim(swd) 
+swd <- rbind(as.data.frame(fe.gbif.df), sample.df.keep); dim(swd) 
 
 
 pa <- c(rep(1, nrow(fe.gbif.df)), rep(0, nrow(sample.df.keep))); length(pa) # presence/absence vector
@@ -281,7 +279,7 @@ AUC # 0.8198168
 threshold(eval)
 plot(eval, 'ROC')
 
-# single response bio02
+# single response bio02 #BIO2 = Mean Diurnal Range (Mean of monthly (max temp - min temp))
 head(swd); str(swd)
 swd.bio02 <- as.data.frame(swd[,c('bio02', 'mask')])
 head(swd.bio02)
@@ -290,7 +288,10 @@ me.bio02 <- maxent(swd.bio02, pa, args = c("noproduct", "nothreshold", "nohinge"
 me.bio02
 response(me.bio02, 'bio02')
 
+# single response bio16 #BIO16 = Precipitation of Wettest Quarter
 
+### Null-model ###
+## Run null-model from source
 
 ### 7. SDM on ISRIC Soil within climate niche ####
 
